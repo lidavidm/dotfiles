2f536a91d8e033bd4f418afd5a8d9fbb5e24a913
David Li <li.davidm96@gmail.com>
2022-10-30 14:37:08 -0400

    Update init.el

--- init.el
+++ init.el
@@ -4,20 +4,22 @@
 ;;; Code:
 
 (require 'package)
-(push '("marmalade" . "http://marmalade-repo.org/packages/")
-      package-archives )
 (push '("melpa" . "https://melpa.org/packages/")
       package-archives)
 (package-initialize)
 
 (add-to-list 'load-path "~/.emacs.d/lisp")
 
+;; For lsp-mode
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (* 4 1024 1024)) ;; 4mb
+
 ;; GUI settings
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (setq frame-title-format "%b - emacs")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-;; (xterm-mouse-mode 1)
+
+;; Some minor modes
 
 (add-hook 'text-mode-hook
           (lambda ()
@@ -30,18 +32,17 @@
 
 (global-auto-revert-mode 1)
 
-;; Some minor modes
+(global-undo-tree-mode)
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'prog-mode-hook 'flyspell-prog-mode)
 
 ;; ace-window
 (global-set-key (kbd "C-x o") 'ace-window)
 
-;; Powerline
-;; (require 'powerline)
-;; (powerline-center-evil-theme)
-;; ;; Fix issue with alt-tab not resetting colors
-;; (remove-hook 'focus-out-hook 'powerline-unset-selected-window)
+;; yasnippet
+(yas-global-mode 1)
+
+;; doom-modeline
 
 (require 'doom-modeline)
 (doom-modeline-mode 1)
@@ -53,30 +54,14 @@
 ;; Indentation
 (setq-default indent-tabs-mode nil)
 
-;; ivy
+;; selectrum
 
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-
-(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> l") 'counsel-find-library)
-(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-(global-set-key (kbd "C-c k") 'counsel-ag)
-(define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-
-;;; make ivy like ido
-(define-key ivy-minibuffer-map (kbd "C-j") #'ivy-immediate-done)
-(define-key ivy-minibuffer-map (kbd "RET") #'ivy-alt-done)
-
-(setq ivy-wrap 't)
+(selectrum-mode +1)
+(selectrum-prescient-mode +1)
+(prescient-persist-mode +1)
 
 ;; projectile
+
 (require 'projectile)
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
@@ -86,7 +71,6 @@
 (electric-pair-mode 1)
 
 ;; whitespace
-;; (global-whitespace-mode 1)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; fixmes
@@ -106,6 +90,10 @@ This functions should be added to the hooks of major modes for programming."
 (require 'evil)
 (evil-mode 1)
 (setq-default evil-cross-lines t)
+(evil-define-key 'normal 'global
+  "g+" 'evil-numbers/inc-at-pt
+  "g=" 'evil-numbers/inc-at-pt
+  "g-" 'evil-numbers/dec-at-pt)
 ;; http://stackoverflow.com/a/32660401/262727
 
 (eval-when-compile (require 'cl))
@@ -122,12 +110,6 @@ This functions should be added to the hooks of major modes for programming."
 (advice-add 'evil-beginning-of-line :around (evil--check-visual-line-mode 'evil-beginning-of-visual-line))
 (advice-add 'evil-end-of-line :around (evil--check-visual-line-mode 'evil-end-of-visual-line))
 
-;; (require 'evil-little-word)
-;; (define-key evil-motion-state-map (kbd "w") 'evil-forward-little-word-begin)
-;; (define-key evil-motion-state-map (kbd "glw") 'evil-forward-word-begin)
-;; (define-key evil-motion-state-map (kbd "b") 'evil-backward-little-word-begin)
-;; (define-key evil-motion-state-map (kbd "glb") 'evil-backward-word-begin)
-
 (defun comment-or-uncomment-region-or-line ()
     "Comments or uncomments the region or the current line if there's no active region."
     (interactive)
@@ -137,22 +119,13 @@ This functions should be added to the hooks of major modes for programming."
             (setq beg (line-beginning-position) end (line-end-position)))
         (comment-or-uncomment-region beg end)))
 
-(define-key evil-normal-state-map (kbd ";") 'comment-or-uncomment-region-or-line)
-(define-key evil-normal-state-map (kbd "C-.") 'ido-imenu-anywhere)
-(define-key evil-normal-state-map (kbd "/") 'swiper)
-(define-key evil-normal-state-map (kbd "?") 'swiper)
-
-(define-key evil-visual-state-map (kbd ";") 'comment-or-uncomment-region-or-line)
 (define-key evil-insert-state-map (kbd "C-d") 'evil-delete-char)
 (define-key evil-insert-state-map (kbd "M-d") 'kill-word)
-(define-key evil-insert-state-map (kbd "C-.") 'ido-imenu-anywhere)
+(define-key evil-normal-state-map (kbd ";") 'comment-or-uncomment-region-or-line)
+(define-key evil-visual-state-map (kbd ";") 'comment-or-uncomment-region-or-line)
 
 (require 'evil-surround)
 (global-evil-surround-mode 1)
-
-;; (require 'evil-mc)
-;; (global-evil-mc-mode 1)
-;; (evil-define-key 'normal map (kbd "C-p") 'evil-paste-pop)
 
 (evil-escape-mode 1)
 (setq-default evil-escape-key-sequence "fd")
@@ -164,20 +137,9 @@ This functions should be added to the hooks of major modes for programming."
 
 ;; flycheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
-;;; needed for flychcek to check whether the crate is binary or library
-(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
 
 ;; company-mode
 (add-hook 'after-init-hook 'global-company-mode)
-
-;; clojure-mode
-(add-hook 'clojure-mode-hook '(lambda () (cider-mode 1)))
-
-;; rust
-;; (add-hook 'rust-mode-hook 'cargo-minor-mode)
-(add-hook 'rust-mode-hook '(lambda ()
-                             ;; (racer-mode 1)
-                             (eldoc-mode 1)))
 
 ;; javascript: js2
 
@@ -200,33 +162,18 @@ This functions should be added to the hooks of major modes for programming."
 
 (add-to-list 'auto-mode-alist `(,(rx ".svelte" string-end) . web-mode))
 
-;; typescript: tide
-
-(add-hook 'typescript-mode-hook
-          (lambda ()
-            (tide-setup)
-            (flycheck-mode +1)
-            (setq flycheck-check-syntax-automatically '(save mode-enabled))
-            (company-mode-on)))
-(add-to-list 'auto-mode-alist `(,(rx ".tsx" string-end) . web-mode))
-(add-to-list 'auto-mode-alist `(,(rx ".facade" string-end) . web-mode))
-
 ;; aligns annotation to the right hand side
 (setq company-tooltip-align-annotations t)
 
-;; haskell-mode
-
-;; latex-mode
-(add-hook 'LaTeX-mode-hook (lambda ()
-                             (flyspell-mode 1)
-                             (visual-line-mode 1)))
-
-;; C++/CXX/C
+;; C/C++
 
 (require 'google-c-style)
 (add-hook 'c++-mode-hook
           (lambda ()
-            (google-set-c-style)))
+            (google-set-c-style)
+            ;; turn off flycheck since it's not very useful to us
+            (flycheck-mode 0)
+            (lsp-deferred)))
 
 ;; Markdown
 
@@ -240,35 +187,9 @@ This functions should be added to the hooks of major modes for programming."
 
 (add-hook 'org-mode-hook
           (lambda ()
-            (org-indent-mode t)
             (visual-line-mode t)
-            ;; (adaptive-wrap-prefix-mode t)
             (flyspell-mode t)
             ))
-
-(org-babel-do-load-languages 'org-babel-load-languages '((latex . t)))
-
-(eval-after-load "ox-latex"
-  ;; update the list of LaTeX classes and associated header (encoding, etc.)
-  ;; and structure
-  '(add-to-list 'org-latex-classes
-                `("beamer"
-                  ,(concat "\\documentclass[presentation]{beamer}\n"
-                           "[DEFAULT-PACKAGES]"
-                           "[PACKAGES]"
-                           "[EXTRA]\n")
-                  ("\\section{%s}" . "\\section*{%s}")
-                  ("\\subsection{%s}" . "\\subsection*{%s}")
-                  ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))))
-(setq org-latex-listings t)
-;; (add-to-list 'org-latex-packages-alist '("" "listings"))
-
-;; pollen
-
-(add-hook 'pollen-mode-hook
-          (lambda ()
-            (flyspell-mode t)))
-(add-to-list 'auto-mode-alist `(,(rx ".pm" string-end) . pollen-mode))
 
 ;; adaptive-wrap when available
 
@@ -278,10 +199,13 @@ This functions should be added to the hooks of major modes for programming."
     (adaptive-wrap-prefix-mode (if visual-line-mode 1 -1)))
   (add-hook 'visual-line-mode-hook 'my-activate-adaptive-wrap-prefix-mode))
 
-;; variable-pitch-mode
-;; (add-hook 'text-mode-hook
-;;           (lambda ()
-;;             (variable-pitch-mode 1)))
+;; macros for Arrow development
+
+(fset 'assign-or-raise
+   (kmacro-lambda-form [?i ?A ?R ?R ?O ?W ?_ ?A ?S ?S ?I ?G ?N ?_ ?O ?R ?_ ?R ?A ?I ?S ?E ?\( delete escape ?f ?= ?a backspace backspace ?, escape ?f ?\; ?s ?\) ?\; escape] 0 "%d"))
+
+(fset 'return-not-ok
+   (kmacro-lambda-form [?v ?% ?S ?\) ?i ?R ?E ?T ?U ?R ?N ?_ ?N ?O ?T ?_ ?O ?K escape] 0 "%d"))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -301,10 +225,13 @@ This functions should be added to the hooks of major modes for programming."
  '(completion-ignored-extensions
    '(".o" "~" ".bin" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyo" ".log"))
  '(custom-safe-themes
-   '("37a4701758378c93159ad6c7aceb19fd6fb523e044efe47f2116bc7398ce20c9" "6096a2f93610f29bf0f6fe34307587edd21edec95073cbfcfb9d7a3b9206b399" "d6c5b8dc6049f2e9dabdfcafa9ef2079352640e80dffe3e6cc07c0f89cbf9748" "ce3e6c12b48979ce89754884d913c7ecc8a7956543d8b09ef13abfab6af9aa35" "fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" "9d9fda57c476672acd8c6efeb9dc801abea906634575ad2c7688d055878e69d6" "1c082c9b84449e54af757bcae23617d11f563fc9f33a832a8a2813c4d7dfb652" "8f83397e58db2a094d534aef83bbecbeefa1fb473141e7504609dc07580b66af" "7527f3308a83721f9b6d50a36698baaedc79ded9f6d5bd4e9a28a22ab13b3cb1" "4cbec5d41c8ca9742e7c31cc13d8d4d5a18bd3a0961c18eb56d69972bbcf3071" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "a2dd771a05705be2a6e6adb6ddbc7a27ebf49edab1dffdbefe243096becba7c9" "71ecffba18621354a1be303687f33b84788e13f40141580fa81e7840752d31bf" "561ba4316ba42fe75bc07a907647caa55fc883749ee4f8f280a29516525fc9e8" "a81bc918eceaee124247648fc9682caddd713897d7fd1398856a5b61a592cb62" default))
+   '("e27556a94bd02099248b888555a6458d897e8a7919fd64278d1f1e8784448941" "37a4701758378c93159ad6c7aceb19fd6fb523e044efe47f2116bc7398ce20c9" "6096a2f93610f29bf0f6fe34307587edd21edec95073cbfcfb9d7a3b9206b399" "d6c5b8dc6049f2e9dabdfcafa9ef2079352640e80dffe3e6cc07c0f89cbf9748" "ce3e6c12b48979ce89754884d913c7ecc8a7956543d8b09ef13abfab6af9aa35" "fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" "9d9fda57c476672acd8c6efeb9dc801abea906634575ad2c7688d055878e69d6" "1c082c9b84449e54af757bcae23617d11f563fc9f33a832a8a2813c4d7dfb652" "8f83397e58db2a094d534aef83bbecbeefa1fb473141e7504609dc07580b66af" "7527f3308a83721f9b6d50a36698baaedc79ded9f6d5bd4e9a28a22ab13b3cb1" "4cbec5d41c8ca9742e7c31cc13d8d4d5a18bd3a0961c18eb56d69972bbcf3071" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "a2dd771a05705be2a6e6adb6ddbc7a27ebf49edab1dffdbefe243096becba7c9" "71ecffba18621354a1be303687f33b84788e13f40141580fa81e7840752d31bf" "561ba4316ba42fe75bc07a907647caa55fc883749ee4f8f280a29516525fc9e8" "a81bc918eceaee124247648fc9682caddd713897d7fd1398856a5b61a592cb62" default))
  '(doc-view-continuous t)
+ '(ess-style 'RStudio)
+ '(evil-undo-system 'undo-tree)
  '(flycheck-checkers
    '(ada-gnat asciidoctor asciidoc c/c++-gcc c/c++-cppcheck cfengine chef-foodcritic coffee coffee-coffeelint coq css-csslint css-stylelint cwl d-dmd dockerfile-hadolint emacs-lisp emacs-lisp-checkdoc erlang-rebar3 erlang eruby-erubis fortran-gfortran go-gofmt go-golint go-vet go-build go-test go-errcheck go-unconvert go-megacheck groovy haml handlebars haskell-stack-ghc haskell-ghc haskell-hlint html-tidy javascript-eslint javascript-jshint javascript-standard json-jsonlint json-python-json jsonnet less less-stylelint llvm-llc lua-luacheck lua perl perl-perlcritic php php-phpmd php-phpcs processing proselint protobuf-protoc pug puppet-parser puppet-lint python-flake8 python-pylint python-pycompile r-lintr racket rpm-rpmlint markdown-markdownlint-cli markdown-mdl nix rst-sphinx rst ruby-rubocop ruby-reek ruby-rubylint ruby ruby-jruby rust-cargo rust rust-clippy scala scala-scalastyle scheme-chicken scss-lint scss-stylelint sass/scss-sass-lint sass scss sh-bash sh-posix-dash sh-posix-bash sh-zsh sh-shellcheck slim slim-lint sql-sqlint systemd-analyze tcl-nagelfar tex-chktex tex-lacheck texinfo typescript-tslint verilog-verilator vhdl-ghdl xml-xmlstarlet xml-xmllint yaml-jsyaml yaml-ruby))
+ '(flycheck-python-pycompile-executable "python3")
  '(font-latex-fontify-sectioning 1.0)
  '(gofmt-command "goimports")
  '(hl-sexp-background-color "#1c1f26")
@@ -312,6 +239,7 @@ This functions should be added to the hooks of major modes for programming."
  '(jabber-account-list '(("david@genki.is" (:password . "CWr2jexUWcvuzMyLLEhI"))))
  '(js2-highlight-level 3)
  '(js2-mode-assume-strict t)
+ '(lsp-clients-clangd-args '("--header-insertion-decorators=0"))
  '(org-agenda-files nil)
  '(org-html-container-element "section")
  '(org-html-divs
@@ -324,11 +252,14 @@ This functions should be added to the hooks of major modes for programming."
  '(org-preview-latex-default-process 'imagemagick)
  '(org-startup-folded nil)
  '(package-selected-packages
-   '(protobuf-mode pollen-mode ripgrep doom-modeline prettier cider company cython-mode cmake-mode counsel-projectile projectile js2-mode leuven-theme rust-mode merlin merlin-eldoc eglot doom-themes poet-theme js2-refactor org-bullets rainbow-delimiters nord-theme ace-window counsel ivy moe-theme ag imenu-anywhere org-ref htmlize ox-reveal color-theme-sanityinc-tomorrow white-theme adaptive-wrap company-go go-eldoc go-guru go-mode lua-mode company-racer material-theme yaml-mode web-mode tuareg tide smex racket-mode racer powerline paredit multiple-cursors markdown-mode magit json-mode ido-vertical-mode haskell-mode goto-last-change glsl-mode flycheck-rust flycheck-elm fill-column-indicator expand-region evil-vimish-fold evil-surround evil-rsi evil-mc evil-escape elm-yasnippets elm-mode deferred cyberpunk-theme cargo auctex alchemist))
+   '(prescient selectrum selectrum-prescient mermaid-mode yasnippet evil-numbers string-inflection ess undo-tree lsp-mode flycheck protobuf-mode pollen-mode ripgrep doom-modeline prettier cider company cython-mode cmake-mode counsel-projectile projectile js2-mode leuven-theme rust-mode merlin merlin-eldoc eglot doom-themes poet-theme js2-refactor org-bullets rainbow-delimiters nord-theme ace-window counsel ivy moe-theme ag imenu-anywhere org-ref htmlize ox-reveal color-theme-sanityinc-tomorrow white-theme adaptive-wrap company-go go-eldoc go-guru go-mode lua-mode company-racer material-theme yaml-mode web-mode tuareg tide smex racket-mode racer powerline paredit multiple-cursors markdown-mode magit json-mode ido-vertical-mode haskell-mode goto-last-change glsl-mode flycheck-rust flycheck-elm fill-column-indicator expand-region evil-vimish-fold evil-surround evil-rsi evil-mc evil-escape elm-yasnippets elm-mode deferred cyberpunk-theme cargo auctex alchemist))
  '(racer-cmd "/home/lidavidm/.cargo/bin/racer")
+ '(ripgrep-executable "/home/lidavidm/.nix-profile/bin/rg")
  '(rust-format-on-save t)
  '(safe-local-variable-values
-   '((js2-global-externs . import)
+   '((eval add-to-list 'lsp-clients-clangd-args "--query-driver=/home/lidavidm/miniconda3/envs/dev/bin/g++")
+     (nxml-child-indent . 4)
+     (js2-global-externs . import)
      (js2-include-node-externs . t)
      (js2-include-node-externs quote t)
      (eval prettier-mode t)
@@ -350,8 +281,8 @@ This functions should be added to the hooks of major modes for programming."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 105 :family "JetBrains Mono")))))
-
+ '(default ((t (:height 105 :family "JetBrains Mono"))))
+ '(selectrum-prescient-primary-highlight ((t (:foreground "black" :underline t :weight bold)))))
 
 (if (display-graphic-p)
     (load-theme 'doom-flatwhite))
